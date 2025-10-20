4074cd876a35f8c910b6bcf1f1c2381a622c57aa
Bikramjit Chowdhury <bkrrulez@gmail.com>
2025-10-20 13:33:17 +0000

    provide simple sql script which I can run directly on db to remove const

--- scripts/migrations/002_update_absence_types.sql
+++ scripts/migrations/002_update_absence_types.sql
@@ -1,6 +1,6 @@
--- First, remove the old constraint
-ALTER TABLE public.absences DROP CONSTRAINT absences_type_check;
+-- First, remove the existing constraint from the 'absences' table.
+ALTER TABLE absences DROP CONSTRAINT IF EXISTS absences_type_check;
 
--- Then, add the new constraint with the updated values
-ALTER TABLE public.absences ADD CONSTRAINT absences_type_check 
+-- Then, add a new constraint that includes the 'In Office' and 'Home Office' types.
+ALTER TABLE absences ADD CONSTRAINT absences_type_check 
 CHECK (type = ANY (ARRAY['General Absence'::text, 'Sick Leave'::text, 'In Office'::text, 'Home Office'::text]));
